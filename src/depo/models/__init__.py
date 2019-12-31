c64ab279a9074573b8c5ba2c5d5691747693d310
Hutton, Rob <Rob.Hutton@primerica.com>
2019-12-31 13:00:55 -0500

    Add roles to config item

--- src/depo/models/__init__.py
+++ src/depo/models/__init__.py
@@ -1,4 +1,6 @@
 from .ConfigItemTree import ConfigItemTree
 from .ConfigItem import ConfigItem
+from .ConfigItem import ConfigItemConfigItemRole
 from .ConfigItemAttribute import ConfigItemAttribute
 from .ConfigItemAttributeValue import ConfigItemAttributeValue
+from .ConfigItemRole import ConfigItemRole
