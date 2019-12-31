585eee63356ca247a50c89f3377871324fd20439
Hutton, Rob <Rob.Hutton@primerica.com>
2019-12-31 12:57:31 -0500

    Add verbose_name to app config

--- src/depo/__init__.py
+++ src/depo/__init__.py
@@ -1,1 +1,2 @@
 # module
+default_app_config = 'depo.apps.depoConfig'
--- src/depo/apps.py
+++ src/depo/apps.py
@@ -1,4 +1,6 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
-class DepoConfig(AppConfig):
+class depoConfig(AppConfig):
     name = 'depo'
+    verbose_name = _("Configuration Management Database")
