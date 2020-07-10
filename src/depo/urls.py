634380110dbaaed7442220915eb232f812d9b928
Rob Hutton <justlikeef@gmail.com>
2020-07-10 17:22:17 -0400

    Update url for jstree to remove jstree

--- src/depo/urls.py
+++ src/depo/urls.py
@@ -19,7 +19,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('jstree/', include('django_jstree.urls')),
+    path('', include('django_jstree.urls')),
     path('admin/', admin.site.urls),
     path('', views.index)
 ]
